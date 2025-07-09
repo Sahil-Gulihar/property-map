bbf1c2fc077290a9f2737ea6a797c66aeb62c086
Sahil Gulihar <sahilgullihar@gmail.com>
2025-07-09 12:25:43 +0530

    minor-changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Himanshi Property Demo App',
+  description: 'Himanshi property demo app',
   generator: 'v0.dev',
 }
 
